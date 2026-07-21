0eabdc105203e665359e02e0bccb903a18d41f36
vpgits <103498292+vpgits@users.noreply.github.com>
2026-07-21 14:59:50 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        tracker = {}
+        for word in strs:
+            counts = [0] * 26
+            for letter in word:
+                index = ord(letter) - ord("a")
+                counts[index] += 1
+            hashedCounts = tuple(counts)
+            if hashedCounts in tracker:
+                tracker[hashedCounts] = [*tracker[hashedCounts], word]
+            else:
+                tracker[hashedCounts] = [word]
+        return list(tracker.values())
+            
+
+        
